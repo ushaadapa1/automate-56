25e85984ab7d4076bd9829cdab57cf4cf22d6915
satya <smandadapu.mail@gmail.com>
2023-10-17 09:43:59 +0530

    changes from Dev branch

--- src/main/java/jar/App.java
+++ src/main/java/jar/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!-update from dev branch" );
+System.out.println( "changes from Dev branch" );
+    }
     }
 }
