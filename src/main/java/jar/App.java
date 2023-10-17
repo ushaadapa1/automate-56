6525a21a0b27c0c089fea43c429e078abfee6323
satya <smandadapu.mail@gmail.com>
2023-10-17 09:46:37 +0530

    change from feature branch

--- src/main/java/jar/App.java
+++ src/main/java/jar/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!-feature changes" );
+System.out.println( "changes from feature branch" );
     }
 }
